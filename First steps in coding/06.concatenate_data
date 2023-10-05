da31676e7acdb7dafa3483b2e3f6eedb0b32ad01
Atanas Kolev <145436664+kovalsk1@users.noreply.github.com>
2023-10-05 15:28:38 +0300

    Delete First steps in coding directory

--- First steps in coding/06.concatenate_data
+++
@@ -1,6 +0,0 @@
-name = input()
-last_name = input()
-age = int(input())
-town = input()
-
-print(f"You are {name} {last_name}, a {age}-years old person from {town}.")
--- First steps in coding/07.projects_creation.py
+++
@@ -1,6 +0,0 @@
-name = input()
-count_projects = int(input())
-
-needed_hours = count_projects * 3
-
-print(f"The architect {name} will need {needed_hours} hours to complete {count_projects} project/s.")
